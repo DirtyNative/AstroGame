5451b7b1ec2a9213be2a06013a80bff73edd3258
Daniel Martin <daniel@zookeeper.app>
2021-02-24 15:05:15 +0100

    Reimplemented abstract classes

--- AstroGame.Shared/Models/StellarThing.cs
+++ AstroGame.Shared/Models/StellarThing.cs
@@ -6,11 +6,22 @@ namespace AstroGame.Shared.Models
     /// <summary>
     /// Defines anything that is available in Cosmos
     /// </summary>
-    public interface StellarThing
+    public abstract class StellarThing
     {
-        Guid Id { get; set; }
-        Guid? ParentId { get; set; }
+        public Guid Id { get; set; }
+        public Guid? ParentId { get; set; }
 
-        string Name { get; set; }
+        public virtual string Name { get; set; }
+
+        public virtual StellarSystem Parent { get; set; }
+
+        protected StellarThing()
+        {
+        }
+
+        protected StellarThing(StellarSystem parent)
+        {
+            Parent = parent;
+        }
     }
 }
