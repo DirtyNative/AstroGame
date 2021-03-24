9d802043ab290ee65f21541e5deee2f4b816b750
Daniel Martin <daniel@zookeeper.app>
2021-03-24 18:26:24 +0100

    Removed Views and ViewModels from IdentityServer

--- Backend/AstroGame/Communication/Models/PlayerResponse.cs
+++ Backend/AstroGame/Communication/Models/PlayerResponse.cs
@@ -9,7 +9,6 @@ namespace AstroGame.Api.Communication.Models
     public class PlayerResponse
     {
         public Guid Id { get; set; }
-        public string Email { get; set; }
         public string Username { get; set; }
         public Guid? PlayerSpeciesId { get; set; }
 
